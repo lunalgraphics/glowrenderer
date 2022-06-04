8684cd2764e5015480c2d568cdaa4787008b2d7a
yikuansun <yikuansun2015@gmail.com>
2022-06-04 08:55:35 -0400

    Make sliders
    nonfunctional

---
+++ sliders.js
@@ -0,0 +1,16 @@
+var numberInputs = document.querySelectorAll("input[type=number]");
+for (var nuin of numberInputs) {
+    var row = nuin.parentElement.parentElement;
+    var tr = document.createElement("tr");
+    row.insertAdjacentElement("afterend", tr);
+    var td = document.createElement("td");
+    td.setAttribute("colspan", 2);
+    tr.appendChild(td);
+    var slider = document.createElement("input");
+    slider.setAttribute("type", "range");
+    slider.setAttribute("value", nuin.getAttribute("value"));
+    slider.setAttribute("min", nuin.getAttribute("min"));
+    slider.setAttribute("max", nuin.getAttribute("max"));
+    slider.setAttribute("step", nuin.getAttribute("step"));
+    td.appendChild(slider);
+}
