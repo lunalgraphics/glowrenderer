46e077d0e2e4e0d63544b055834ce8d791becf06
yikuansun <yikuansun2015@gmail.com>
2022-06-04 09:15:27 -0400

    Make slider adjacent to nuin

--- sliders.js
+++ sliders.js
@@ -1,18 +1,12 @@
 var numberInputs = document.querySelectorAll("input[type=number]");
 for (var nuin of numberInputs) {
-    var row = nuin.parentElement.parentElement;
-    var tr = document.createElement("tr");
-    row.insertAdjacentElement("afterend", tr);
-    var td = document.createElement("td");
-    td.setAttribute("colspan", 2);
-    tr.appendChild(td);
     var slider = document.createElement("input");
     slider.setAttribute("type", "range");
     slider.setAttribute("min", nuin.getAttribute("min"));
     slider.setAttribute("max", nuin.getAttribute("max"));
     slider.setAttribute("step", nuin.getAttribute("step"));
     slider.setAttribute("value", nuin.getAttribute("value"));
-    td.appendChild(slider);
+    nuin.insertAdjacentElement("beforebegin", slider);
     slider.dataset.ID = nuin.id;
     slider.addEventListener("input", function() {
         document.getElementById(this.dataset.ID).value = this.value;
@@ -21,6 +15,7 @@ for (var nuin of numberInputs) {
         var evt = new Event("change");
         document.getElementById(this.dataset.ID).dispatchEvent(evt); // extreme value theorem
     });
-    td.style.textAlign = "center";
     slider.style.accentColor = "var(--special-color)";
+    slider.style.width = "80px";
+    slider.parentElement.style.width = "143px";
 }
